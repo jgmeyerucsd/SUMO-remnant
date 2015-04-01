f3457ce75088abfd05ac28af6b6855325e4e0ae8
Jesse <jgmeyer@ucsd.edu>
2015-04-01 15:06:59 -0700

    added comments to mzIDfix

--- mzIDfix.R
+++ mzIDfix.R
@@ -1,28 +1,40 @@
+
+#### set current working directory to the location of your mzid file
 setwd("C:/Users/JgMeyer/Documents/R/pepsum/pepsum")
 
+### make an R object containing file names that have mzid suffix
 files<-list.files(pattern="mzid")
+#### print the files in the above object
 files
+### load required external libraries
 library(mzID)
 library(XML)
-test<-xmlTreeParse(files, useInternal = TRUE, , asText=TRUE)
+#test<-xmlTreeParse(files, useInternal = TRUE, , asText=TRUE)
+
+### prints usage for parse command
 ?xmlParse
 
-### parse and gind peptide elements
+### parse and find peptide elements
+### set the number in brackets to the correct file number in the "files" object
 doc <- xmlParse(files[3])
 r = xmlRoot(doc) #gives content of root
-r[[3]]["Peptide"]  ### gives me a list of the peptide nodes
 
-r[[3]]["Peptide"][[1]]  ### gives me the first peptide element 
+#r[[3]]["Peptide"]  ### gives me a list of the peptide nodes
+
+#r[[3]]["Peptide"][[1]]  ### gives me the first peptide element 
 ### output from the above line
 #<Peptide id="Pep1">
 #  <PeptideSequence>GGSRNMGGPYGGGNYGPGGSGGSGGYGGRSRY</PeptideSequence>
 #</Peptide> 
 
-typeof(r[[3]]["Peptide"][[1]])  ### reports type "externalptr"
+#typeof(r[[3]]["Peptide"][[1]])  ### reports type "externalptr"
 
-typeof(r[[3]]["Peptide"][[1]])
+#typeof(r[[3]]["Peptide"][[1]])
+
+### get the peptide tags and store in tags object
 tags <- xmlElementsByTagName(r[[3]], "Peptide")
 
+#### function to fix all tags, run all lines in order
 manipulate <- function(tag) {
     ## get 'Modification' node set
     dMod <- tag["Modification"]
@@ -42,73 +54,12 @@ manipulate <- function(tag) {
     tag
 }
 
-lapply(tags, manipulate)
+#### this command takes about 20 minutes, R appears to freeze but it does not actually, just let it work until you can interact with the GUI again
+lapply(tags, manipulate)   ### applies manipulate to the XML structure to fix tags
 ?saveXML
 
-saveXML(r)
+#saveXML(r)
+#### saves new XML file
 saveXML(r, file="dh_fix.mzid", indent=TRUE)
-xmlSize(r)
-xmlName(r[[1]])
-xmlName(r[[2]])
-xmlName(r[[3]]["Peptide"])
-xmlName(r[[1]])
-xmlName(r[[2]])
-xmlName(r[[3]])
 
-
-
-xmltop2 = xmlRoot(test)
-pepelem<-xmlElementsByTagName(xmltop[[3]],name="Peptide")
-pepelem2<-xmlElementsByTagName(xmltop2[[3]],name="Peptide")
-
-pepelem[[1]]
-typeof(pepelem[[1]])
-
-
-
-pepelem2[[1239]]
-
-typeof(pepelem2[[1]])
-
-
-testchildren<-xmlChildren(xmltop[[3]])
-typeof(testchildren)
-testchildren$peptide
-names(testchildren)=="Peptide"
-
-xmlElementSummary(xmlfile)
-?readLines
-
-## Parse the XML file
-doc <- xmlTreeParse("sample.xml", useInternal = TRUE)
-## Select the nodes we want to update
-nodes <- getNodeSet(test, "//peptide")
-## For each node, apply gsub on the content of the node
-lapply(nodes, function(n) {
-  xmlValue(n) <- gsub("ABC","foobar",xmlValue(n))
-})
-
-
-nct_url <- "http://clinicaltrials.gov/ct2/show/NCT00112281?resultsxml=true"
-xml_doc <- xmlParse(nct_url, useInternalNode=TRUE)
-
-mod_path <- "/MzIdentML/peptide/modification" 
-mod_text <- xpathSApply(xmlfile, mod_path, xmlValue)
-
-
-# read in to a tree:
-x = xmlParse("test.xml")
-
-# this returns a *list* of text nodes under sequence
-# and NOT the text nodes under taxon
-nodeSet = xpathApply(x,"//sequence/text()")
-nodeSet = xpathApply(xmlfile,"//SequenceCollection/text()")
-
-# now we loop over the list returned, and get and modify the node value:
-sapply(nodeSet,function(G){
-  text = paste("CCCCC",xmlValue(G),"GGGGGGG",sep="")
-  text = gsub("[^A-Z]","",text)
-  xmlValue(G) = text
-})
-
-
+### now have mzid file with tags fixed
